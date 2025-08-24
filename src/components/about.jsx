59ceca6f320367bbc851af111281314aca079f9e
kmb8043 <you@example.com>
2025-08-24 15:25:10 -0400

    update about me

--- src/components/about.jsx
+++ src/components/about.jsx
@@ -7,7 +7,8 @@ const About = () => {
             <h1 className="text-4xl font-bold text-center text-[#001b5e]">About Me</h1>
             <img className='rounded-full h-auto max-w-xs' src='src\assets\Kim.jpg' />
                 <p className=" display-inline text-center py-8">
-                    I was 31 years old when I decided to make a career change. I studied criminal justice in college and jumped straight into working for the Federal Government as a Security Officer and a Law Enforcement Officer. <p> I discovered coding through my older sister and my husbands brother. I played around on Khan Academy and watch many youtube videos to get my feet wet. </p> <p>I have always loved learning new skills and challenging myself. Coding gives me the mental challenge everyday.</p>
+                    Hi I am Kimberly. I am a full- stack developer. I have always loved learning new skills and challenging myself. I discovered I had a passion for web-development one day just playing around with Code Academy. 
+                    <p> Going from a career in the Federal Goverment to a life of coding was a little nerve racking at first. </p> <p> Coding challenges me mentally everyday and I love it.</p>
                 </p>  
         </div>
     )
